76b5320f50da518f20b55c2d696917aa8fd25127
Ravi Prajapati <raviprajapati@promactinfo.com>
2021-09-30 11:07:30 +0530

    Dynamic menu form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-
+import { Component} from '@angular/core';
+import { FormGroup, FormBuilder, FormArray } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,131 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'dynamic-menu';
+
+  constructor(private formBuilder: FormBuilder) { }
+
+  dynamicMenuForm: FormGroup;
+  dynamicMenu:any
+
+  //Menu Object
+  menuObj = [
+    {
+      subItems: []
+    }
+  ];
+
+  // Create Form 
+  createForm(obj) {
+    var arr = [];
+    for (var i = 0; i < obj.length; i++) {
+      arr.push(this.createItem(obj[i]));
+    }
+    this.dynamicMenuForm = this.formBuilder.group({
+      items: this.formBuilder.array(arr)
+    });
+  }
+  
+  createItem(obj): FormGroup {
+    var subArr = [];
+    for (var i = 0; i < obj.subItems.length; i++) {
+      subArr.push(this.createSubItem(obj.subItems[i]));
+    }
+    return this.formBuilder.group({
+      name: '',
+      subItems: this.formBuilder.array(subArr)
+    });
+  }
+
+  createSubItem(subItem): FormGroup {
+    var subArr = [];
+    for (var i = 0; i < subItem.subItems2.length; i++) {
+      subArr.push(this.createSubItem2(subItem.subItems2[i]));
+    }
+    return this.formBuilder.group({
+      subname: subItem.subname,
+      subItems2: this.formBuilder.array(subArr)
+    });
+  }
+
+  createSubItem2(subItem): FormGroup {
+    var subArr = [];
+    for (var i = 0; i < subItem.subItems3.length; i++) {
+      subArr.push(this.createSubItem3(subItem.subItems3[i]));
+    }
+    return this.formBuilder.group({
+      subname2: subItem.subname2,
+      subItems3: this.formBuilder.array(subArr)
+    });
+  }
+
+  createSubItem3(subItem): FormGroup {
+    return this.formBuilder.group({
+      subname3: subItem.subname3
+    });
+  }
+
+  createSingleItem(subItem): FormGroup {
+    return this.formBuilder.group({
+      subname: subItem.subname,
+      subItems2: this.formBuilder.array([])
+    });
+  }
+
+  //Add First Menu
+  addSubItems(item: FormGroup, i): void {
+    let value = item.get('name').value
+    let subItems = item.get("subItems") as FormArray;
+    var newSubItem = {
+      subname: value
+    };
+    subItems.push(this.createSingleItem(newSubItem));
+   
+  }
+
+  //Add SubMenu
+  addSubItems2(item: FormGroup, i): void {
+    let subItems = (item.get("subItems") as FormArray).controls[i].get(
+      "subItems2"
+    ) as FormArray;
+    var newSubItem = {
+      subname2: "",
+      subItems3: this.formBuilder.array([])
+    };
+    subItems.push(this.createSubItem2(newSubItem));
+  }
+  //Add Nested SubMenu
+  addSubItems3(item: FormGroup, i): void {
+    let subItems = (item.get("subItems2") as FormArray).controls[i].get(
+      "subItems3"
+    ) as FormArray;
+    var newSubItem = {
+      subname3: "",
+    };
+    subItems.push(this.createSubItem3(newSubItem));
+  }
+
+  // Delete All Submenu by index
+  removeItem(item, i) {
+    const form = item.get("subItems") as FormArray;
+    form.removeAt(i);
+  }
+  removeItem2(item, i) {
+    const form = item.get("subItems2") as FormArray;
+    form.removeAt(i);
+  }
+  removeItem3(item, i) {
+    const form = item.get("subItems3") as FormArray;
+    form.removeAt(i);
+  }
+  get formControls() {
+    return (this.dynamicMenuForm.get('items') as FormArray).controls;
+  }
+  //Save changes to See Menu in Navbar
+  saveChanges(){
+    this.dynamicMenu = this.dynamicMenuForm.value; 
+  }
+  ngOnInit() {
+    this.createForm(this.menuObj);
+    this.dynamicMenu = this.dynamicMenuForm.value; 
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+// RECOMMENDED
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+// import { DynammicComponent } from './dynammic/dynammic.component';
+// import { DynammicItemComponent } from './dynammic-item/dynammic-item.component';
+// import { DynammicItemLastComponent } from './dynammic-item-last/dynammic-item-last.component';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BsDropdownModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
